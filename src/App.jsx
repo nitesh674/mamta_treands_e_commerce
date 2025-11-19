cc9bef527e50665b6ca3f01977ba9843199fe764
nitesh674 <kurmaverma391@gmail.com>
2025-11-19 20:34:08 +0530

    add page loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Audio } from "react-loader-spinner";
 import { useState, useEffect } from "react";
 
 import Navbar from "./components/Navbar";
@@ -17,6 +18,13 @@ import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000)
+  }, []);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/cart", { credentials: "include" })
@@ -68,8 +76,27 @@ function App() {
   const cartCount = cart.reduce((s, it) => s + (it.qty || 0), 0);
   return (
     <>
+    { loader ? (
+           <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#e4868593",
+        }}
+      >
+           <Audio
+            height={80}
+            width={80}
+            radius={9}
+            color="white"
+            ariaLabel="audio-loading"
+          />
+        </div>
+      ) : (
+      <>   
       <Navbar cartCount={cartCount} />
-
       <Routes>
         <Route path="/"
           element={
@@ -79,22 +106,18 @@ function App() {
               <CardProduct addToCart={addToCart} />
               <NewArrivals addToCart={addToCart} />
               <ClientSection addToCart={addToCart} />
-              {/* <Footer /> */}
             </>
           } />
         <Route path="/about" element={<About />} />
-        <Route path="/products"  element={<Products addToCart={addToCart} />} />
+        <Route path="/products" element={<Products addToCart={addToCart} />} />
         <Route path="/ClientSection" element={<ClientSection />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/product/:id" element={<ProductDetail addToCart={addToCart} />} />
-        {/*   <Route path="/blog" element={<Blog />} /> */}
-
         <Route path="/cart" element={<CartSection cart={cart} setCart={setCart} />} />
       </Routes>
-
-      {/* <CardProduct addToCart={addToCart} /> */}
-
       <Footer />
+       </>
+    )}
     </>
   );
 }
