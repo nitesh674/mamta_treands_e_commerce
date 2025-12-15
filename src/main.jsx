92c95626bc5d3540b9db6808af8949ac51a5b624
nitesh674 <kurmaverma391@gmail.com>
2025-12-15 21:21:56 +0530

    edit to cartsection

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Loader from "./components/loader.jsx";
+import Loader from "./components/Loader.jsx";
 import { CartProvider } from "./context/CartContext";
 
 // Bootstrap CSS + JS (Bootstrap 5)
